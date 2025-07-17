782c6346486e7cd16660964b8c0f7676ea87b838
Sumit0716 <35055857+Sumit0716@users.noreply.github.com>
2025-07-17 14:26:35 +0530

    Done

---
+++ field_dictionary.py
@@ -0,0 +1,10 @@
+# field_dictionary.py
+
+FIELD_DICT = {
+    'Region': 'REGION',
+    'Category': 'CATEGORY',
+    'Sales': 'TOTAL SALES',
+    'Profit': 'PROFIT (USD)',
+    'Year': 'ORDER YEAR',
+    'Product': 'PRODUCT NAME'
+}
